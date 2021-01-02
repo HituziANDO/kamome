f45a75f3ee36b45bda0886ac879c6f1e418cc52d
Masaki Ando <hhh.ando@gmail.com>
2021-01-02 14:04:48 +0900

    [Android] The sample app converted Java to Kotlin.

---
+++ android/app/src/main/java/jp/hituzi/kamome/sample/MainActivity.kt
@@ -0,0 +1,63 @@
+package jp.hituzi.kamome.sample
+
+import android.app.Activity
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.webkit.WebView
+import android.widget.ImageButton
+import jp.hituzi.kamome.Command
+import jp.hituzi.kamome.NativeClient
+import jp.hituzi.kamome.NativeClient.SendMessageCallback
+import java.util.*
+
+class MainActivity : Activity() {
+
+    companion object {
+        private const val TAG = "KamomeSample"
+    }
+
+    private var client: NativeClient? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        actionBar?.hide()
+
+        val webView = findViewById<WebView>(R.id.webView)
+
+        // Creates the NativeClient object with the webView.
+        client = NativeClient(webView)
+            .add(Command("echo") { commandName, data, completion ->
+                // Received `echo` command.
+                // Then sends resolved result to the JavaScript callback function.
+                val map = HashMap<String?, Any?>()
+                map["message"] = data!!.optString("message")
+                completion.resolve(map)
+            })
+            .add(Command("echoError") { commandName, data, completion ->
+                // Sends rejected result if failed.
+                completion.reject("Echo Error!")
+            })
+            .add(Command("tooLong") { commandName, data, completion ->
+                // Too long process...
+                Handler().postDelayed({ completion.resolve() }, (30 * 1000).toLong())
+            })
+
+        client?.howToHandleNonExistentCommand = NativeClient.HowToHandleNonExistentCommand.REJECTED
+
+        webView.loadUrl("file:///android_asset/www/index.html")
+
+        val sendButton = findViewById<ImageButton>(R.id.sendButton)
+        sendButton.setOnClickListener {
+            // Sends a data to the JS code.
+            val data = HashMap<String?, Any?>()
+            data["greeting"] = "Hello! by Kotlin"
+            client?.send(data, "greeting", SendMessageCallback { commandName, result, error ->
+                // Received a result from the JS code.
+                Log.d(TAG, "result: $result")
+            })
+        }
+    }
+}
