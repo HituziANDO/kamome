99ab843c18f8cc5571207ab3097b1a0341231b54
Hituzi Ando <hhh.ando@gmail.com>
2021-10-19 17:03:16 +0900

    Update package

--- js/kamome/webpack.mix.js
+++ js/kamome/webpack.mix.js
@@ -1,10 +1,4 @@
 const mix = require('laravel-mix');
 
 mix.setResourceRoot('');
-mix.js('src/lib/kamome.js', 'public/')
-    // .sass('src/sass/*.scss', 'public/css/')
-    .browserSync({
-        files: './src/lib/*',
-        server: './public/',
-        proxy: false
-    });
+mix.js('src/lib/kamome.js', 'public/');
