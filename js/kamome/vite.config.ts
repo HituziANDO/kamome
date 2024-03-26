13483c9584abf0f547c764eb63a1ddc3a616ff79
Hituzi Ando <hhh.ando@gmail.com>
2024-03-26 17:47:33 +0900

    Create Vite project for kamome

---
+++ js/kamome/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'KM',
+      fileName: (format) => `index.${format}.js`
+    },
+    rollupOptions: {
+      external: [],
+      output: {
+        globals: {}
+      }
+    }
+  }
+});
--- js/kamome/webpack.mix.js
+++
@@ -1,4 +0,0 @@
-const mix = require('laravel-mix');
-
-mix.setResourceRoot('');
-mix.js('src/lib/kamome.js', 'public/');
